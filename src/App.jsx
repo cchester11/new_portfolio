811e8b42fb3526e17a4d6d4c27aa155be3600c8b
Charles <cchester1@angelo.edu>
2025-03-16 22:52:03 -0500

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,38 @@ import Footer from "./components/Footer";
 
 function App() {
       return (
-        <div className="h-screen w-screen">
-          <nav className="flex justify-around align-top w-full p-5 pt-7">
-                  <button type="button">
-                        <a href="" target="_blank">
-                              <img src="./bioSmall.png"></img>
-                        </a>
-                  </button>
-                  <button type="button">
-                        <a href="" target="_blank">
-                              <img src="./articleSmall.png"></img>
-                        </a>
-                  </button>
-                  <button type="button">
-                        <a href="https://github.com/cchester11" target="_blank">
-                              <img src="./githubSmall.png"></img>
-                        </a>
-                  </button>
-                  <button type="button">
-                        <a href="https://www.linkedin.com/in/chrlchester/" target="_blank">
-                              <img src="./linkedinSmall.png"></img>
-                        </a>
-                  </button>
-            </nav>
-            <Projects />
-            <Footer />
-        </div>
+            <div className="h-screen w-screen">
+                  <nav className="flex justify-around align-top w-full p-5 pt-7">
+                        <button type="button">
+                              <a href="" target="_blank">
+                                    <img src="./bioSmall.png"></img>
+                              </a>
+                        </button>
+                        <button type="button">
+                              <a href="" target="_blank">
+                                    <img src="./articleSmall.png"></img>
+                              </a>
+                        </button>
+                        <button type="button">
+                              <a
+                                    href="https://github.com/cchester11"
+                                    target="_blank"
+                              >
+                                    <img src="./githubSmall.png"></img>
+                              </a>
+                        </button>
+                        <button type="button">
+                              <a
+                                    href="https://www.linkedin.com/in/chrlchester/"
+                                    target="_blank"
+                              >
+                                    <img src="./linkedinSmall.png"></img>
+                              </a>
+                        </button>
+                  </nav>
+                  <Projects />
+                  <Footer />
+            </div>
       );
 }
 
