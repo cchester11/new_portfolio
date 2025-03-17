d7b3fd3cf6c789c8cd3ab34c4c31850d95ae5ce7
Charles <cchester1@angelo.edu>
2025-03-17 11:01:05 -0500

    about and posts added to

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,12 @@ import { useState } from "react";
 
 function About () {
       return (
-            <div>
-                  About Me
+            <div className="flex flex-wrap p-20 gap-10 flex-col justify-center text-cyan-50">
+                  <h2>My name is Charles Chester</h2>
+                  <br></br>
+                  <p>I graduated from the Univeristy of Texas with a bootcamp certification in full stack web development.</p>
+                  <br></br>
+                  <p>Since then I have worked as a freelancer building applications for small companies.</p>
             </div>
       )
 };
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 
 function Posts () {
       return (
-            <div>Posts</div>
+            <div className="flex flex-wrap p-20 gap-10 flex-col justify-center text-cyan-50">
+                  <h2>Posts</h2>
+            </div>
       )
 };
 
