c9606081a8f4c131b047ac8e62f65367c6772f89
Charles <cchester1@angelo.edu>
2025-03-18 09:47:18 -0500

    added post_details

--- src/data.js
+++ src/data.js
@@ -21,17 +21,40 @@ const projects = [
             screenshot: "phopApp",
             link: "https://github.com/cchester11/phop_frontend"
       }
-]
+];
 
 export const posts = [
       {
             id: "1",
-            title: "async await"
+            title: "why so many semi colons?"
       },
       {
             id: "2",
             title: "building my usage monitor"
       }
-]
+];
+
+export const post_details = [
+      {
+            // each object contains an id and a content array
+            id: "1",
+            content: [
+                  // each content array contains nested objects which contain an element type (like h2) and content
+                  {
+                        type: "h2",
+                        content: "Why So Many Semi Colons?"
+                  }
+            ]
+      },
+      {
+            id: "2",
+            content: [
+                  {
+                        type: "h2",
+                        content: "Building My Usage Monitor"
+                  }
+            ]
+      }
+];
 
 export default projects;
