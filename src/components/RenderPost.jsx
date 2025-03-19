8f04d49e512ac75144a91472546d6f91482aa61f
Charles <cchester1@angelo.edu>
2025-03-19 17:54:19 -0500

    changes to format of RenderPost component

--- src/components/RenderPost.jsx
+++ src/components/RenderPost.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { post_details } from "../data";
 
@@ -7,31 +7,32 @@ function RenderPost() {
       const [contents, setContents] = useState([]);
 
       // algorithm to find correct post
-      useMemo(() => {
-            for (let i = 0; i < post_details.length; i++) {
-                  if (post_details[i].id === params.id) {
-                        setContents([...post_details[i].content])
-                  }
+      useEffect(() => {
+            const foundPost = post_details.find(
+                  (post) => post.id === params.id
+            );
+            if (foundPost) {
+                  setContents([...foundPost.content]);
             }
-      }, [params.id]);
+      }, [params.id]); // <- dependency to avoid unnecessary updates
 
       return (
             <div className="flex flex-wrap p-20 gap-10 flex-col justify-center text-cyan-50">
                   {contents.map((item, index) => {
                         switch (item.type) {
                               case "h2":
-                                    return <h2 
-                                                key={index} 
+                                    return (
+                                          <h2
+                                                key={index}
                                                 className="flex justify-center border-b-1 border-b-cyan-50"
                                           >
                                                 {item.content}
-                                          </h2>;
-                              case "p": 
-                                    return <p 
-                                                key={index}
-                                          >
-                                                {item.content}
-                                          </p>
+                                          </h2>
+                                    );
+                              case "p":
+                                    return <p key={index}>{item.content}</p>;
+                              default:
+                                    return null;
                         }
                   })}
             </div>
