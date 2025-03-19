e6841e9f09575affc0785a41186899ba1e0ac8cf
Charles <cchester1@angelo.edu>
2025-03-19 09:07:18 -0500

    renderpost component created

---
+++ src/components/RenderPost.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function RenderPost () {
+      return (
+            <div>Render post here</div>
+      )
+};
